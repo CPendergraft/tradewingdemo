e193fa8fee7f300509abe4280541c52da79da7ae
CPendergraft <chris@sickluggage.com>
2021-01-20 16:21:27 -0800

    Added a fix on validation

--- src/_hooks/useForm.js
+++ src/_hooks/useForm.js
@@ -11,22 +11,24 @@ const useForm = (callback, validate) => {
         if (errors.valid === true && isSubmitting) {
             callback();
         }
-    }, [errors, callback, isSubmitting]);
+
+    }, [errors, callback, isSubmitting, values]);
 
     const handleSubmit = (event) => {
         if (event) event.preventDefault();
         console.log(values);
         setErrors(validate(values));
         setIsSubmitting(true);
+        errors.valid = false;
     };
     const handleInput = (event) =>{
         console.log('handleInput', event.target.value);
         event.persist();
-        setValues(values => ({ ...values, [event.target.name]: event.target.value }, () => setErrors(validate(values)) ));
+        setValues(values => ({ ...values, [event.target.name]: event.target.value }  ));
 
         if(errors.thisyear){
 
-            setSelectedValue(0, () => setErrors(validate(values)) );
+            setSelectedValue(0 );
         }
         if(errors.lastyear){
             setSelectedValue(1);
@@ -35,11 +37,13 @@ const useForm = (callback, validate) => {
             setSelectedValue(2);
         }
         if(errors.tenyears){
-            setSelectedValue(10);
+            setSelectedValue(3);
         }
         if(!errors.tenyears && !errors.twoyears && !errors.lastyear && errors.thisyear){
             setSelectedValue(12);
         }
+
+        setErrors(validate(values))
 
     };
     const handleSelect = (event) => {
@@ -50,19 +54,19 @@ const useForm = (callback, validate) => {
 
         if(val==='0'){
             console.log('case1');
-            setValues({yearstart:today, yearend:today}, setErrors(validate(values)));
+            setValues({yearstart:today, yearend:today});
         }else if(val==='1'){
             console.log('case2');
-            setValues({yearstart:(today-1), yearend:(today-1)}, setErrors(validate(values)));
+            setValues({yearstart:(today-1), yearend:(today-1)});
         }else if(val==='2'){
             console.log('case3');
-            setValues({yearstart:(today-1), yearend:(today)},setErrors(validate(values)));
+            setValues({yearstart:(today-1), yearend:(today)});
         }else if(val==='10'){
             console.log('case4');
-            setValues({yearstart:(today-10), yearend:(today)},setErrors(validate(values)));
+            setValues({yearstart:(today-10), yearend:(today)});
         }
 
-
+     setErrors(validate(values))
 
 
 
