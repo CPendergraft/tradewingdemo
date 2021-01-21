9bfd1aeb8916341eca7bc2a2514d9795f8aa6012
CPendergraft <chris@sickluggage.com>
2021-01-20 17:54:26 -0800

    Added message for start>end

--- src/_hooks/YearRangeValidator.js
+++ src/_hooks/YearRangeValidator.js
@@ -56,7 +56,7 @@ export default function validate(values) {
 
         console.log('validate',parseInt(values.yearstart), parseInt(values.yearend));
 
-
+        errors.yearstart = 'Start date must proceed end date';
         errors.disabled = true;
         if(values.yearend === values.yearstart){
 
