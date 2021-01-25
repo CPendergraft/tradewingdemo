6d3cf7c1f52fc09244a2fa8173dafd7bb95e4869
CPendergraft <chris@sickluggage.com>
2021-01-25 14:16:20 -0800

    removed unneeded state wrapper

--- src/_components/YearRangeComponent.js
+++ src/_components/YearRangeComponent.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 
 const YearRangeForm = (props) => {
 
 
 
-    const [selectionHandler] = useState(props.selectionHandler);
+    const selectionHandler =  props.selectionHandler;
 
 
     const {
